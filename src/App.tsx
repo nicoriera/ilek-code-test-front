ba413e0c55ec01255646fbcbadb3464babd8326e
hercemer42 <brian@brianfox.fr>
2023-05-28 10:20:50 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [questions, setQuestions]: [any, any] = useState();
+
+  useEffect(() => {
+    fetch("/environment_questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Teste tes connaissances</h1>
+      <p>{JSON.stringify(questions)}</p>
     </div>
   );
 }
