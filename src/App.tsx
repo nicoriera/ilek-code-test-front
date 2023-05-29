d2822aa43b477eb67d9003aaba966a29862c642f
hercemer42 <brian@brianfox.fr>
2023-05-29 11:36:55 +0200

    fix type

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [questions, setQuestions]: [any, any] = useState();
+  const [questions, setQuestions] = useState<any>();
 
   useEffect(() => {
     fetch("/environment_questions")
